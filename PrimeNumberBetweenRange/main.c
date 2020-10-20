42ee3f922ee3efefcbde1de1b1609c024a87eff7
Prajwal Dhungana <dhunganaprajwal1@gmail.com>
2020-10-20 18:09:31 -0500

    Initial Commit

---
+++ PrimeNumberBetweenRange/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  PrimeNumberBetweenRange
+//
+//  Created by Prajwal Dhungana on 10/20/20.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
